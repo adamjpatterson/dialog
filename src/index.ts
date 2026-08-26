f734fa1376bafaaf131648a34bb5e91bd742ed88
Adam Patterson <adpatter@umich.edu>
2026-08-26 15:49:13 -0400

    Fix exports.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,81 @@
 export { log, formatter, consoleHandler, SyslogLevel } from "./commons/logger.js";
 export { TwilioVoIP } from "./implementations/voip/twilio/twilio_voip.js";
-export { TwilioGateway, TwilioGatewayOptions } from "./implementations/voip/twilio/twilio_gateway.js";
-export { TwilioVoIPWorker } from "./implementations/voip/twilio/twilio_voip_worker.js";
+export { TwilioVoIP as TwilioVoIPInterface } from "./interfaces/voip/twilio_voip.js";
+export {
+  TwilioGateway,
+  TwilioGatewayOptions,
+  TwilioGatewayEvents,
+  WebSocketListener,
+  WebSocketListenerOptions,
+} from "./implementations/voip/twilio/twilio_gateway.js";
+export { TwilioVoIPOptions } from "./implementations/voip/twilio/twilio_voip.js";
+export { TwilioVoIPWorker, TwilioVoIPWorkerOptions } from "./implementations/voip/twilio/twilio_voip_worker.js";
 export { TwilioVoIPProxy } from "./implementations/voip/twilio/twilio_voip_proxy.js";
-export { TranscriptStatus, TwilioMetadata } from "./implementations/voip/twilio/types.js";
+export {
+  Body,
+  CallMetadata,
+  isCallMetadata,
+  RecordingStatus,
+  isRecordingStatus,
+  TranscriptStatus,
+  isTranscriptStatus,
+  WebSocketMessage as TwilioWebSocketMessage,
+  StartWebSocketMessage,
+  isStartWebSocketMessage,
+  MediaWebSocketMessage,
+  isMediaWebSocketMessage,
+  StopWebSocketMessage,
+  isStopWebSocketMessage,
+  MarkWebSocketMessage,
+  isMarkWebSocketMessage,
+  TwilioMetadata,
+} from "./implementations/voip/twilio/types.js";
 export { OpenAIAgent, OpenAIAgentOptions } from "./implementations/agent/abstract/openai/openai_agent.js";
 export { DeepgramSTT, DeepgramSTTOptions } from "./implementations/stt/deepgram/deepgram_stt.js";
+export {
+  LiveClientMessage,
+  LiveClientSpeechStartedMessage,
+  LiveClientResultsMessage,
+  LiveClientUtteranceEndMessage,
+  isResultsMessage,
+  isSpeechStartedMessage,
+  isUtteranceEndMessage,
+} from "./implementations/stt/deepgram/types.js";
 export { CartesiaTTS, CartesiaTTSOptions } from "./implementations/tts/cartesia/cartesia_tts.js";
-export { ElevenlabsTTS } from "./implementations/tts/elevenlabs/elevenlabs_tts.js";
+export {
+  WebSocketMessage as CartesiaWebSocketMessage,
+  TimestampsWebSocketMessage,
+  isTimestampsWebSocketMessage,
+  ChunkWebSocketMessage,
+  isChunkWebSocketMessage,
+  DoneWebSocketMessage,
+  isDoneWebSocketMessage,
+  ErrorWebSocketMessage as CartesiaErrorWebSocketMessage,
+  isErrorWebSocketMessage,
+} from "./implementations/tts/cartesia/types.js";
+export { ElevenlabsTTS, ElevenlabsTTSOptions } from "./implementations/tts/elevenlabs/elevenlabs_tts.js";
+export {
+  WebSocketMessage as ElevenlabsWebSocketMessage,
+  AudioOutputWebSocketMessage,
+  isAudioOutputWebSocketMessage,
+  FinalOutputWebSocketMessage,
+  isFinalOutputWebSocketMessage,
+} from "./implementations/tts/elevenlabs/types.js";
 export { OpenAISTT, OpenAISTTOptions } from "./implementations/stt/openai/openai_stt.js";
-export { Session } from "./implementations/stt/openai/types.js";
-export { StreamBuffer } from "./commons/stream_buffer.js";
+export {
+  Session,
+  WebSocketMessage as OpenAISTTWebSocketMessage,
+  CompletedWebSocketMessage,
+  isCompletedWebSocketMessage,
+  SpeechStartedWebSocketMessage,
+  isSpeechStartedWebSocketMessage,
+  InputAudioTranscriptionDeltaWebSocketMessage,
+  isInputAudioTranscriptionDeltaWebSocketMessage,
+  ConversationItemCreatedWebSocketMessage,
+  isConversationItemCreatedWebSocketMessage,
+} from "./implementations/stt/openai/types.js";
+export { StreamBuffer, StreamBufferOptions } from "./commons/stream_buffer.js";
+export { Mutex, MutexOptions } from "./commons/mutex.js";
 export { TTSEvents, TTS } from "./interfaces/tts/tts.js";
 export { STTEvents, STT } from "./interfaces/stt/stt.js";
 export { Agent } from "./interfaces/agent/agent.js";
